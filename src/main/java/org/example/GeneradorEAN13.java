f013a03106e6cdb82bb27be264c9d3c7a833a6b7
Dani <daniel.ojeda@telefonica.net>
2022-06-29 00:01:15 +0200

    Subida a github el barras

---
+++ src/main/java/org/example/GeneradorEAN13.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class GeneradorEAN13 {
+
+    public static void generarean13(Producto p) throws IOException, WriterException {
+
+        String direccion = "productos/" + p.getEAN13() + "-" + p.getNombre() + ".png";
+        EAN13Writer ean13writer = new EAN13Writer();
+        BitMatrix bitMatrix = ean13writer.encode(p.getEAN13(), BarcodeFormat.EAN_13, 300, 100);
+        Path path = FileSystems.getDefault().getPath(direccion);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+}
